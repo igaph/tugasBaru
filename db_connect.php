7518249d1cd2b59277c5cdb2cf4a073f97611c2e
igaph <34225527+igaph@users.noreply.github.com>
2023-04-25 11:18:44 +0700

    Add files via upload

---
+++ db_connect.php
@@ -0,0 +1,45 @@
+<?php
+class database
+{
+    var $host = "localhost";
+    var $username = "root";
+    var $password = "";
+    var $database = "db_kampus";
+    var $koneksi;
+
+    function __construct()
+    {
+        $this->koneksi = mysqli_connect($this->host, $this->username, $this->password, $this->database);
+    }
+
+    function register($username, $password, $nama)
+    {
+        $insert = mysqli_query($this->koneksi, "insert into user values ('', '$username', '$password', '$nama')");
+        return $insert;
+    }
+
+    function login($username, $password, $remember)
+    {
+        $query = mysqli_query($this->koneksi, "Select * From user Where username='$username'");
+        $data_user = $query->fetch_array();
+        if (password_verify($password, $data_user['password'])) {
+            if ($remember) {
+                setcookie('username', $username, time() + (60 * 60 * 24 * 5), '/');
+                setcookie('nama', $data_user['nama'], time() + (60 * 60 * 24 * 5), '/');
+            }
+            $_SESSION['username'] = $username;
+            $_SESSION['nama'] = $data_user['nama'];
+            $_SESSION['is_login'] = TRUE;
+            return TRUE;
+        }
+    }
+
+    function relogin($username)
+    {
+        $query = mysqli_query($this->koneksi, "Select * from user Where username='$username'");
+        $data_user = $query->fetch_array();
+        $_SESSION['username'] = $username;
+        $_SESSION['nama'] = $data_user['nama'];
+        $_SESSION['is_login'] = TRUE;
+    }
+}
